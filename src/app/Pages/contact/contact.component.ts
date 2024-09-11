7a89b0e1585d49997359bb83a23cbb6761d0c103
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-11 12:04:46 +0200

    added navbar to all pages

--- src/app/Pages/contact/contact.component.ts
+++ src/app/Pages/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../UI/Shared-UI/nav/nav.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
--- src/app/Pages/settings/settings.component.ts
+++ src/app/Pages/settings/settings.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../UI/Shared-UI/nav/nav.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
