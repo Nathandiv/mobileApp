1fb2ab2379013036de5ee91c1334c27dcc06e822
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-16 15:21:39 +0200

    added screen resistraction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'mobileapp';
-
+screen =window.innerWidth
   ngOnInit(): void {
     initFlowbite();
   }
