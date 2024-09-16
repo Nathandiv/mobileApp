ac72b89d5c177cbd4d21efcf2414502eda818c43
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-16 12:34:06 +0200

    made changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,11 +8,12 @@ import { SettingsComponent } from './Pages/settings/settings.component';
 import { Error404Component } from './Pages/error404/error404.component';
 
 export const routes: Routes = [
+  {path:'',component:LandingPpageComponent},
   {path: 'home', component: HomeComponent},
   {path: 'about', component:AboutComponent},
   {path:'contact',component:ContactComponent},
   {path:'settings',component:SettingsComponent},
   {path:'**',component:Error404Component},
-  {path:'',component:LandingPpageComponent}
+
 
 ];
